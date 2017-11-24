5849a8c269fbebed6bbcb8ea760a91f8a3d81c47
d <lizard43@gmail.com>
2017-11-24 17:32:15 -0500

    updates

--- trivia/index.js
+++ trivia/index.js
@@ -1,20 +1,11 @@
-var jsonServer = require('json-server');
-var fs = require('fs');
-var https = require('https');
-var jsonServer = require('json-server');
-
+var jsonServer = require('json-server'),
 server = jsonServer.create(),
 router = jsonServer.router('trivia/trivia.json'),
 middlewares = jsonServer.defaults();
 
-var options = {
-    key: fs.readFileSync('ssl/key.pem'),
-    cert: fs.readFileSync('ssl/cert.pem')
-};
-    
 server.use(middlewares);
 server.use(router);
 
-https.createServer(options, server).listen(3000, function() {    
-console.log("json-server started on port " + 3000);
+server.listen(process.env.PORT || 5000, function() {
+console.log("json-server started on port " + process.env.PORT || 5000);
 });
